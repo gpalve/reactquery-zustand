c78de817d7f9c122fc763d648423126b4b3f821a
Ganesh Palve <gpalve@gmail.com>
2023-05-10 13:48:21 +0530

    Implemented Infinite scrolling

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,17 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import ReactFragment from "react";
 import usePosts from "./hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading..</p>;
   if (error) return <p>{error.message}</p>;
 
   return (
     <>
-      <select
+      {/* <select
         onChange={(event) => setUserId(parseInt(event.target.value))}
         value={userId}
         className="form-select mb-3"
@@ -20,14 +22,25 @@ const PostList = () => {
         <option value="1">User 1</option>
         <option value="2">User 2</option>
         <option value="3">User 3</option>
-      </select>
+      </select> */}
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
+      <button
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
+        className="btn btn-success my-2"
+      >
+        {isFetchingNextPage ? "Loading.." : "Load More..."}
+      </button>
     </>
   );
 };
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import axios from "axios"
 
 interface Post {
@@ -7,17 +7,26 @@ interface Post {
     body: string;
     userId: number;
   }
+interface PostQuery {
+    pageSize: number;
+}
+const usePosts = (query:PostQuery) => {
 
-const usePosts = (userId : number | undefined) => {
-
-    const fetchPosts = () => axios
-    .get<Post[]>('https://jsonplaceholder.typicode.com/posts',{params:{userId}})
+    const fetchPosts = ({pageParam=1}) => axios
+    .get<Post[]>('https://jsonplaceholder.typicode.com/posts',{params:{
+        _start : (pageParam-1)*query.pageSize,
+        _limit : query.pageSize
+    }})
     .then((res) => (res.data))
 
-     const posts = useQuery<Post[],Error>({
-        queryKey: userId ?  ["users",userId,"posts"] : ["posts"],
-        queryFn:fetchPosts,
-        staleTime:1*60*1000 // 1 min
+     const posts = useInfiniteQuery<Post[],Error>({
+        queryKey: ["posts",query],
+        queryFn:({pageParam}) => fetchPosts({pageParam}),
+        staleTime:1*60*1000, // 1 min
+        keepPreviousData:true,
+        getNextPageParam:(lastPage,allPages) => {
+            return lastPage.length > 0 ? allPages.length + 1 : undefined
+        }
      })
 
      return posts
